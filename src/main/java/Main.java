e073efc42a615a00896107d41aa8d1499f166e67
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-09-20 09:20:14 -0300

    Caso de ejemplo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    public static void main(String[] args) {
+        int[] arreglo = {1, -4, 2, 2, 5, -1};
+        int maxProducto = productoAdyacentes(arreglo);
+        System.out.println("El producto adyacente es: " + maxProducto);
+    }
+
+    public static int productoAdyacentes(int[] arr) {
+        if (arr.length < 2) {
+            throw new IllegalArgumentException("El arreglo debe tener al menos dos elementos");
+        }
+
+        int maxProducto = arr[0] * arr[1];
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            int productoActual = arr[i] * arr[i + 1];
+            if (productoActual > maxProducto) {
+                maxProducto = productoActual;
+            }
+        }
+
+        return maxProducto;
+    }
 }
